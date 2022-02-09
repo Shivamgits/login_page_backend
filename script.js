de01bb4295d95797960742151b93f2a519d7b381
shivam singh <92594066+Shivamgits@users.noreply.github.com>
2022-02-09 11:27:28 +0530

    Enter

---
+++ script.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = express();
+
+dotenv.config({path: './config.env'});
+require('./conn')
+
+const PORT = process.env.PORT;
+
+
+
+
+
+
+//middleware
+
+// const middleware = (req, res, next) =>{
+//    console.log('middleware')
+
+//    next();
+// }
+
+
+// middleware();
+
+app.get('/',(req, res) => {
+  res.send("hello")
+})
+app.get('/about',(req, res) => {
+  res.send("hello about")
+})
+app.get('/contact',(req, res) => {
+  res.send("hello contact") 
+})
+app.listen(PORT,()=>{
+    console.log(`listening on port http://localhost:${PORT}`)
+})
